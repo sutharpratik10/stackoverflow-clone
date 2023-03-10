74cd95d525ebb75c798f41d5b3500b593a904aff
Pratik <sutharpratik10@hotmail.com>
2023-03-10 10:50:36 +0000

    Firebase_auth integration

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -1,19 +1,70 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import {signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
 import GoogleIcon from '@mui/icons-material/Google';
+import { auth, provider } from '../../firebase';
 import './index.css'
+import { red } from '@mui/material/colors';
 
 const Index = () => {
     const [register, setRegister] = useState(true)
-    const [loading] = useState(false) 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [loading, setLoading] = useState(false) 
+    const [error, setError] = useState("")
+
+    const handleSignInGoogle = () => {
+        signInWithPopup(auth, provider).then((res) => {
+            console.log(res)
+        })
+    }
+    const handleRegister = (e) => {
+        e.preventDefault()
+        setError("")
+        setLoading(true)
+        if(email === "" || password === '' || username === ""){
+            setError("Required field is missing")
+            setLoading(false)
+        }else {
+            createUserWithEmailAndPassword(auth, email, password).then((res) =>  {
+                setLoading(false)
+                console.log(res)
+            }).catch((error)=>{
+                console.log(error)
+                setError(error.message)
+                setLoading(false)
+            })
+        }
+    };
+    const handleSignIn = (e) => {
+        e.preventDefault()
+        setError("")
+        setLoading(true)
+        if(email === "" || password === "")
+        {
+            setError("Required field is missing")
+            setLoading(false)
+        }
+        else {
+            signInWithEmailAndPassword(auth, email, password).then((res) => {
+                console.log(res)
+                setLoading(false)
+            }).catch((error)=> {
+                console.log(error.code)
+                setError(error.message)
+                setLoading(false) 
+            })
+        }
+    }
+
   return (
     <div className='auth'>
         <div className='auth-container'>
             <p>Add another way to log in using any of the following services. </p>
             <div className="sign-options">
-                <div className="single-option">
+                <div onClick={handleSignInGoogle} className="single-option">
                     <span className='logo'><GoogleIcon/></span> 
-                    <p>Login with Google</p>
+                    <p>{loading ? "logging in..." : "Login with Google"} </p>
                 </div>
             </div>
             <div className='auth-login'>
@@ -24,17 +75,16 @@ const Index = () => {
                                 {" "}
                                 <div className="input-field">
                                 <p>Username</p>
-                                <input
-                                    placeholder='username'
-                                    value=''
-                                    type="text"
+                                <input placeholder='username' 
+                                    value={username} onChange={(e) => setUsername(e.target.value)} 
+                                    type="text" 
                                 />
                                 </div>
                                 <div className="input-field">
                                 <p>Email</p>
                                 <input
                                     placeholder='email'
-                                    value=''
+                                    value= {email} onChange={(e)=> setEmail(e.target.value)}
                                     type="email"
                                 />
                                 </div>
@@ -42,11 +92,11 @@ const Index = () => {
                                 <p>Password</p>
                                 <input
                                     placeholder='password'
-                                    value=''
+                                    value= {password} onChange={(e)=> setPassword(e.target.value)}
                                     type="password"
                                 />
                                 </div>
-                                <button onClick='' disabled='' className='auth-button'>{loading ? "Registering..." : "Register"}
+                                <button onClick={handleRegister} disabled='' className='auth-button'>{loading ? "Registering..." : "Register"}
                                 </button>
                             </>
                         ) : 
@@ -57,7 +107,7 @@ const Index = () => {
                                 <p>Email</p>
                                 <input
                                     placeholder='email'
-                                    value=''
+                                    value= {email} onChange={(e)=> setEmail(e.target.value)}
                                     type="email"
                                 />
                                 </div>
@@ -65,11 +115,11 @@ const Index = () => {
                                 <p>Password</p>
                                 <input
                                     placeholder='password'
-                                    value=''
+                                    value={password} onChange={(e)=> setPassword(e.target.value)}
                                     type="password"
                                 />
                                 </div>
-                                <button onClick='' disabled='' className='auth-button'>{loading ? "logging in..." : "login"}
+                                <button onClick= {handleSignIn} disabled='' className='auth-button'>{loading ? "logging in..." : "login"}
                                 </button>
                             </>
                         )
@@ -77,6 +127,9 @@ const Index = () => {
                     <p onClick={() => setRegister(!register)}> {register ? "Login" : "Register"} ? </p>
                 </div>
             </div>
+            {
+                error !== "" && (<p className='errormessage'> {error}</p>)
+            }
         </div>
         
     </div>
